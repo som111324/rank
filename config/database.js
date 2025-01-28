2ef90aef12d368bbf31c687036295dc3414cfc2a
Subham Nayak <nayaksubham2003@gmail.com>
2025-01-28 19:59:52 +0530

    logs changes

--- config/database.js
+++ config/database.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/leaderboard', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
     });
     console.log('Database connected successfully!');
   } catch (error) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers); 
-res.status(200).json({ message: 'User API working!' });
+
 router.post('/', userController.addUser);   
 router.post('/:userId/claim', userController.claimPoints); 
 
